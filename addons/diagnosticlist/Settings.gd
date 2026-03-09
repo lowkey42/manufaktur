721d7fa4270b85dc891c5a32c620e59f0c09467b
BenediktBrei <diers@hochschule-trier.de>
2026-03-09 15:52:06 +0100

    first working menu version

--- addons/diagnosticlist/Settings.gd
+++ addons/diagnosticlist/Settings.gd
@@ -7,14 +7,14 @@ const SETTING_AUTO_REFRESH = BASE_SETTING_PATH + "auto_refresh"
 
 
 static func set_auto_refresh(on: bool) -> void:
-    _set_setting(SETTING_AUTO_REFRESH, on)
+	_set_setting(SETTING_AUTO_REFRESH, on)
 
 
 static func get_auto_refresh() -> bool:
-    return ProjectSettings.get_setting(SETTING_AUTO_REFRESH, true) as bool
+	return ProjectSettings.get_setting(SETTING_AUTO_REFRESH, true) as bool
 
 
 static func _set_setting(name: String, value: Variant) -> void:
-    ProjectSettings.set_setting(name, value)
-    ProjectSettings.set_as_internal(name, true)
-    ProjectSettings.save()
+	ProjectSettings.set_setting(name, value)
+	ProjectSettings.set_as_internal(name, true)
+	ProjectSettings.save()
--- menus/MainMenu.cs
+++ menus/MainMenu.cs
@@ -33,20 +33,20 @@ public partial class MainMenu : Control {
 		_creditsMenu.Visible        = false;
 	}
 
-	public void _on_start_button_button_down() {
+	public void _on_start_button_pressed() {
 		_settingsMenu.Visible       = false;
 		_levelSelectionMenu.Visible = true;
 		_mainMenu.Visible           = false;
 		_creditsMenu.Visible        = false;
 	}
-	public void _on_settings_button_button_down() {
+	public void _on_settings_button_pressed() {
 		_settingsMenu.Visible       = true;
 		_levelSelectionMenu.Visible = false;
 		_mainMenu.Visible           = false;
 		_creditsMenu.Visible        = false;
 	}
 	
-	public void _on_exit_button_button_down() {
+	public void _on_exit_button_button_pressed() {
 		GetTree().Quit();
 
 	}
