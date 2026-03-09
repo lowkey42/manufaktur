ef98796d34ce87928ace56951ee69f4635e97af0
Florian Oetke <lowkey@second-system.de>
2026-03-09 22:47:12 +0100

    feat: spike trap

--- gameplay/GameCore.cs
+++ gameplay/GameCore.cs
@@ -3,11 +3,10 @@ using Godot;
 using Manufaktur.gameplay;
 
 public partial class GameCore : Node {
-
 	[Export] private int _startDelay = 3;
 
 	[Export] private int _ghostHeadStart = 2;
-	
+
 	[Export] private PackedScene _ghostScene;
 
 	[Export] private Hud _hud;
@@ -17,7 +16,7 @@ public partial class GameCore : Node {
 	[Export] private GhostTracker _ghostTracker;
 
 	private HighscoreList _highscoreList;
-	
+
 	private bool _started = false;
 	private float _startTime;
 	private float _finishTime;
@@ -26,13 +25,14 @@ public partial class GameCore : Node {
 	private Ghost _ghost = null;
 
 	public float TimeSeconds => _time;
-	
+
 	public override void _Ready() {
-		_highscoreList = new HighscoreList(GetParentOrNull<Node>()?.GetName() ?? "unknown"); 
-		
+		_highscoreList = new HighscoreList(GetParentOrNull<Node>()?.GetName() ?? "unknown");
+
 		var bus = EventBus.Instance;
 		bus.FinishReached += OnFinished;
-		
+		bus.PlayerDied    += OnPlayerDied;
+
 		var spawnPoints = GetTree().GetNodesInGroup("spawn_point");
 		if (spawnPoints.Count > 0) {
 			if (spawnPoints[Random.Shared.Next(0, spawnPoints.Count)] is Node3D sp) {
@@ -41,14 +41,14 @@ public partial class GameCore : Node {
 		}
 
 		var highscoreEntries = _highscoreList.Entries;
-		if(highscoreEntries.Length > 0)
+		if (highscoreEntries.Length > 0)
 			_hud.SetBestTime(highscoreEntries[0].Time);
-		
+
 		foreach (var entry in highscoreEntries) {
 			var ghostPath = entry.LoadGhost();
 			if (ghostPath == null)
 				continue;
-			
+
 			_ghost = (Ghost) _ghostScene.Instantiate();
 			_ghost.SetPath(ghostPath);
 			AddChild(_ghost, true);
@@ -60,43 +60,41 @@ public partial class GameCore : Node {
 
 	private async void StartCountdown() {
 		var bus = EventBus.Instance;
-		
+
 		_player.ProcessMode = ProcessModeEnum.Disabled;
-		
+
 		for (var i = 0; i < _startDelay; i++) {
 			var timeLeft = _startDelay - i;
-			if(timeLeft <= _ghostHeadStart)
+			if (timeLeft <= _ghostHeadStart)
 				_ghost?.Start();
-			
+
 			_hud.SetCountdown(timeLeft);
 			await ToSignal(GetTree().CreateTimer(1.0), "timeout");
 		}
-		
+
 		_player.ProcessMode = ProcessModeEnum.Inherit;
 		_hud.SetCountdown(0);
-		
+
 		bus.EmitLevelStarted();
-		_started   = true;
+		_started = true;
 	}
 
 	public override void _ExitTree() {
 		_highscoreList.Save();
 		EventBus.Instance.FinishReached -= OnFinished;
+		EventBus.Instance.PlayerDied    -= OnPlayerDied;
 	}
 
 	public override void _Process(double delta) {
 		if (_finishTime == 0 && _started) {
 			_time += (float) delta;
 		}
-		
+
 		_hud.SetCurrentTime(_time);
 		_ghostTracker.SetCurrentTime(_time);
 
 		if (Input.IsActionJustReleased("reset")) {
-			var error = GetTree().ReloadCurrentScene();
-			if(error != Error.Ok) {
-				GD.PrintErr(error);
-			}
+			EventBus.Instance.EmitPlayerDied();
 		}
 	}
 
@@ -111,5 +109,15 @@ public partial class GameCore : Node {
 		GetTree().Paused = true;
 		_hud.ShowHighscore(_highscoreList, entry);
 		Input.SetMouseMode(Input.MouseModeEnum.Visible);
+	}
+
+	private async void OnPlayerDied() {
+		// TODO: play animation/particle-effect and wait for 1-2 seconds
+		await ToSignal(GetTree().CreateTimer(0.2), "timeout");
+
+		var error = GetTree().ReloadCurrentScene();
+		if (error != Error.Ok) {
+			GD.PrintErr(error);
+		}
 	}
 }
