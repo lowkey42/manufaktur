88c552c38f07bb532821cb2036835cae8aece51e
BenediktBrei <diers@hochschule-trier.de>
2026-03-10 13:41:32 +0100

    added highscore menu

--- menus/LevelSelection.cs
+++ menus/LevelSelection.cs
@@ -1,45 +1,54 @@
 using Godot;
-using System;
-
+using Manufaktur.gameplay;
 
 
 public partial class LevelSelection : Control
 {
 	[Signal]
 	public delegate void ClosedEventHandler();
-	
-	[Export]
-	public PackedScene Level1;
-	[Export]
-	public PackedScene Level2;
-	[Export]
-	public PackedScene Level3;
 
+	[Signal]
+	public delegate void ScoreOpenEventHandler(string levelName);
+
+
+	private RichTextLabel _level1Label;
+	private RichTextLabel _level2Label;
+	private RichTextLabel _level3Label;
 
 	public void _on_close_button_pressed() {
-		SettingsManager.Instance.SaveSettings();
 		this.Visible = false;
 		
 		EmitSignal(SignalName.Closed);
 	}
+	
 
-	public async void _on_load_level_1_button_pressed() {
-		await ToSignal(GetTree().CreateTimer(0.3f), SceneTreeTimer.SignalName.Timeout);
-		LoadLevel("res://gameplay/levels/level01.tscn");
-		
-	}
-	public async void _on_load_level_2_button_pressed() {
-		await ToSignal(GetTree().CreateTimer(0.3f), SceneTreeTimer.SignalName.Timeout);
-		LoadLevel("res://gameplay/levels/level01.tscn");
-		
-	}	public async void _on_load_level_3_button_pressed() {
-		await ToSignal(GetTree().CreateTimer(0.3f), SceneTreeTimer.SignalName.Timeout);
-		LoadLevel("res://gameplay/levels/level01.tscn");
-		
+	public override void _Ready()
+	{
+		LoadScores();
 	}
 	
-	public void LoadLevel(string path)
+	private void LoadScores()
 	{
+		_level1Label = GetNode<RichTextLabel>("%Level1ScoreLabel");
+		_level2Label = GetNode<RichTextLabel>("%Level2ScoreLabel");
+		_level3Label = GetNode<RichTextLabel>("%Level3ScoreLabel");
+		
+		var level1 = HighscoreList.LoadTopEntry("level01");
+		var level2 = HighscoreList.LoadTopEntry("level01");
+		var level3 = HighscoreList.LoadTopEntry("level01");
+
+		_level1Label.Text = $"{level1.Name} - {level1.Time}";
+		_level2Label.Text = $"{level2.Name} - {level2.Time}";
+		_level3Label.Text = $"{level3.Name} - {level3.Time}";
+	}
+
+	public void LoadScore(string path) {
+		EmitSignal(SignalName.ScoreOpen, path);
+	}
+	
+	public async void LoadLevel(string path)
+	{
+		await ToSignal(GetTree().CreateTimer(0.3f), SceneTreeTimer.SignalName.Timeout);
 		GetTree().ChangeSceneToFile(path);
 	}
 }
--- menus/MainMenu.cs
+++ menus/MainMenu.cs
@@ -6,6 +6,7 @@ public partial class MainMenu : Control {
 	private LevelSelection _levelSelectionMenu;
 	private Control _mainMenu;
 	private CreditsMenu _creditsMenu;
+	private HighScoreMenu _highScoreMenu;
 	
 
 	public override void _Ready()
@@ -14,10 +15,13 @@ public partial class MainMenu : Control {
 		_levelSelectionMenu = GetNode<LevelSelection>("%LevelSelection");
 		_mainMenu = GetNode<Control>("%MainMenuPanel");
 		_creditsMenu = GetNode<CreditsMenu>("%CreditsMenuPanel");
+		_highScoreMenu = GetNode<HighScoreMenu>("%HighScorePanel");
 		
-		_levelSelectionMenu.Closed += OnMenuClosed;
-		_creditsMenu.Closed        += OnMenuClosed;
-		_settingsMenu.Closed       += OnMenuClosed;
+		_levelSelectionMenu.Closed    += OnMenuClosed;
+		_creditsMenu.Closed           += OnMenuClosed;
+		_settingsMenu.Closed          += OnMenuClosed;
+		_highScoreMenu.Closed         += OnHighScoreClosed;
+		_levelSelectionMenu.ScoreOpen += OnScoreOpened;
 
 		_settingsMenu.Visible       = false;
 		_levelSelectionMenu.Visible = false;
@@ -31,6 +35,24 @@ public partial class MainMenu : Control {
 		_levelSelectionMenu.Visible = false;
 		_mainMenu.Visible           = true;
 		_creditsMenu.Visible        = false;
+		_highScoreMenu.Visible      = false;
+	}
+
+	private void OnScoreOpened(string path) {
+		_highScoreMenu.Visible      = true;
+		_levelSelectionMenu.Visible = false;
+		GD.Print(path);
+		
+		_highScoreMenu._load_score(path);
+	}
+	
+	private void OnHighScoreClosed()
+	{
+		_settingsMenu.Visible       = false;
+		_levelSelectionMenu.Visible = true;
+		_mainMenu.Visible           = false;
+		_creditsMenu.Visible        = false;
+		_highScoreMenu.Visible      = false;
 	}
 
 	public void _on_start_button_pressed() {
@@ -38,17 +60,27 @@ public partial class MainMenu : Control {
 		_levelSelectionMenu.Visible = true;
 		_mainMenu.Visible           = false;
 		_creditsMenu.Visible        = false;
+		_highScoreMenu.Visible      = false;
 	}
 	public void _on_settings_button_pressed() {
 		_settingsMenu.Visible       = true;
 		_levelSelectionMenu.Visible = false;
 		_mainMenu.Visible           = false;
 		_creditsMenu.Visible        = false;
+		_highScoreMenu.Visible      = false;
 	}
 	
 	public void _on_exit_button_button_pressed() {
 		GetTree().Quit();
 
+	}
+	
+	public void _on_high_score_button_pressed() {
+		_settingsMenu.Visible       = false;
+		_levelSelectionMenu.Visible = false;
+		_mainMenu.Visible           = false;
+		_creditsMenu.Visible        = false;
+		_highScoreMenu.Visible      = true;
 	}
 
 	public void _on_credits_button_pressed() {
@@ -56,6 +88,7 @@ public partial class MainMenu : Control {
 		_levelSelectionMenu.Visible = false;
 		_mainMenu.Visible           = false;
 		_creditsMenu.Visible        = true;
+		_highScoreMenu.Visible      = false;
 	}
 	
 }
