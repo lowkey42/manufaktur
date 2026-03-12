22ba78dee30ced89f3d0a5dd570b8a7452a755bd
amy <rbechel.ostmann@googlemail.com>
2026-03-12 20:35:57 +0100

    door sounds added

--- gameplay/interactables/door/Door.cs
+++ gameplay/interactables/door/Door.cs
@@ -8,6 +8,8 @@ public partial class Door : Triggerable
 	AnimationPlayer _animationPlayer;
 
 	[Export] bool DoorOpen { get; set; } = false;
+	[Export] private AudioStreamPlayer3D _openSoundPlayer;
+	[Export] private AudioStreamPlayer3D _closeSoundPlayer;
 
 	public override void _Ready() {
 		base._Ready();
@@ -23,9 +25,11 @@ public partial class Door : Triggerable
 	private void ToggleDoor() {
 		if (DoorOpen) {
 			_animationPlayer.PlayBackwards("Open");
+			_closeSoundPlayer.Play();
 			DoorOpen = false;
 		} else {
 			_animationPlayer.Play("Open");
+			_openSoundPlayer.Play();
 			DoorOpen = true;
 		}
 	}
