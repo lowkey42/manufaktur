bae8af251f4c551aa5a93b7a11381b5054bc6dbd
BenediktBrei <diers@hochschule-trier.de>
2026-03-12 18:49:26 +0100

    added story (now 4 real)

---
+++ menus/TextureSequencer.cs
@@ -0,0 +1,57 @@
+using Godot;
+using System;
+using Manufaktur.menus;
+
+public partial class TextureSequencer : TextureRect
+{
+	[Export] public TextureStep[] Sequence;
+	[Export] public PackedScene NextLevel;
+
+	private int _currentIndex = 0;
+	private float _timer = 0.0f;
+
+	public override void _Ready()
+	{
+		if (Sequence != null && Sequence.Length > 0)
+		{
+			ApplyCurrentStep();
+		}
+	}
+
+	public override void _Process(double delta)
+	{
+		if (Sequence == null || Sequence.Length == 0 || _currentIndex >= Sequence.Length) 
+			return;
+
+		_timer += (float)delta;
+
+
+		if (_timer >= Sequence[_currentIndex].Duration)
+		{
+			_timer = 0.0f;
+			_currentIndex++;
+
+			if (_currentIndex < Sequence.Length)
+			{
+				ApplyCurrentStep();
+			}
+			else
+			{
+				ChangeScene();
+			}
+		}
+	}
+
+	private void ApplyCurrentStep()
+	{
+		var currentStep = Sequence[_currentIndex];
+		if (currentStep != null)
+		{
+			Texture = currentStep.Image;
+		}
+	}
+
+	private void ChangeScene() {
+		GetTree().ChangeSceneToPacked(NextLevel);
+	}
+}
