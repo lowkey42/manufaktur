c0ba8ec7c14307baa1bcef9998abcc89301aff6c
Florian Oetke <lowkey@second-system.de>
2026-03-14 00:26:59 +0100

    feat: integrated credits video

--- menus/CreditsMenu.cs
+++ menus/CreditsMenu.cs
@@ -6,6 +6,22 @@ public partial class CreditsMenu : Control
 	[Signal]
 	public delegate void ClosedEventHandler();
 
+	[Export] private VideoStreamPlayer _video;
+
+	private int _musicVolume;
+
+	public void OnVisibilityChanged() {
+		if (Visible) {
+			var radio = Radio.Instance;
+			_musicVolume = radio.GetMusicVolume();
+			radio.SetMusicVolume(0);
+			_video.Play();
+			
+		} else {
+			_video.Stop();
+			Radio.Instance.SetMusicVolume(_musicVolume);
+		}
+	}
 	
 	private void _on_close_button_pressed() {
 		EmitSignal(SignalName.Closed);
